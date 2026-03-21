295c4dba5c91a0c21c68bbde64e054e74f09e6a6
nerdhitsai-sys <nerdhitsai@gmail.com>
2026-03-21 17:48:13 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
 }));
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('combined'));
 
 // ==========================================
